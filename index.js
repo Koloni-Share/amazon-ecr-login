5bd8ce75e107f08e6d94ee73018c1e9bf6b43b5b
Justin Rubek <justin@koloni.me>
2021-10-21 11:49:47 -0500

    Give password to docker through stdin

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ async function run() {
       // Execute the docker login command
       let doLoginStdout = '';
       let doLoginStderr = '';
-      const exitCode = await exec.exec('docker login', ['-u', creds[0], '-p', creds[1], proxyEndpoint], {
+      const exitCode = await exec.exec(`echo ${creds[1]} | docker login`, ['-u', creds[0], proxyEndpoint], {
         silent: true,
         ignoreReturnCode: true,
         listeners: {
